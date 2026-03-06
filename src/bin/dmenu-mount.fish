4e61132d2a01660f60496bc45dc502efa6f81aa7
Natan Lao <natanlao@users.noreply.github.com>
2026-03-05 21:47:42 -0800

    Suppress system disks

--- src/bin/dmenu-mount.fish
+++ src/bin/dmenu-mount.fish
@@ -1,6 +1,6 @@
 #!/usr/bin/env fish
 
-set disk (ls /dev/disk/by-label | rofi -dmenu -p "Disk")
+set disk (ls /dev/disk/by-label | grep -v boot | grep -v root | grep -v swap | rofi -dmenu -p "Disk")
 set output (udisksctl mount --block-device /dev/disk/by-label/$disk)
 
 if command -q notify-send
--- src/bin/dmenu-umount.fish
+++ src/bin/dmenu-umount.fish
@@ -1,8 +1,14 @@
 #!/usr/bin/env fish
 
-set disk (ls /dev/disk/by-label | rofi -dmenu -p "Disk")
-set output (udisksctl unmount --block-device /dev/disk/by-label/$disk)
+set disk (ls /dev/disk/by-label | grep -v boot | grep -v root | grep -v swap | rofi -dmenu -p "Disk")
+set unmount_output (udisksctl unmount --block-device /dev/disk/by-label/$disk)
+set poweroff_output (udisksctl power-off --block-device /dev/disk/by-label/$disk)
+
 
 if command -q notify-send  # requires libnotify
-    notify-send $output
+    notify-send $unmount_output
+
+    if test $poweroff_output
+        notify-send $poweroff_output
+    end
 end
